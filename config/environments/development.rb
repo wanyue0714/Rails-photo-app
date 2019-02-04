408aa6332401869ab578d14d58e22a5f99f4d80a
wanyue0714 <wanyueglyr@outlook.com>
2019-02-04 15:14:45 -0500

    finish sending email in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://0.0.0.0:3000'}
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
