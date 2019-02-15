8d1ec87c13a01d1e240a04745b086e9ce243c7db
wanyue0714 <wanyueglyr@outlook.com>
2019-02-15 15:50:47 -0800

    finish Image upload functions

---
+++ app/models/image.rb
@@ -0,0 +1,11 @@
+class Image < ApplicationRecord
+  belongs_to :user
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB!")
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_214956) do
+ActiveRecord::Schema.define(version: 2019_02_14_213728) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
